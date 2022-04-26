4daf0a8ed48d9382b1ed2fdacaf0d94d6adbe5b3
Antonio <kombayn_nikoladze@msn.com>
2022-04-26 12:19:08 +0200

    Nivel 1 y 2 completados.

---
+++ src/n2exercici1/Main.java
@@ -0,0 +1,11 @@
+package n2exercici1;
+
+public class Main {
+    public static void main(String[] args) {
+        Smartphone galaxyS20 = new Smartphone("Samsung", "Galaxy S20");
+
+        galaxyS20.trucar("931234121");
+        galaxyS20.fotografiar();
+        galaxyS20.alarmar();
+    }
+}
